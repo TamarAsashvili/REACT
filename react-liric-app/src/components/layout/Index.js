443fa72b15b6441897ca775002ba3124f1b4454d
Tamar Asashvili <asashvili@gmail.com>
2019-05-31 23:56:40 +0200

    lyric

---
+++ react-liric-app/src/components/layout/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tracks from '../tracks/Tracks';
+
+const Index = () => {
+    return (
+        <div>
+            <React.Fragment>
+                <Tracks />
+            </React.Fragment>
+        </div>
+    )
+}
+
+
+export default Index;
